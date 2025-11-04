59262320658ff01381385aa40d850d9e9f4f4ace
0xVantrex <callagansikuche0@gmail.com>
2025-11-04 22:43:07 +0300

    update Devhub.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 //Main controller for the application
+import {loadProfile, saveProfiles } from './storage.js';
+import { renderProfiles, addProfile, editProfile, deleteProfile } from './profile.js';
+import { updateStats } from './stats.js';
+import { searchprofiles } from './search.js';
+import { toggletheme} from './theme.js';
+import { fetchGitHubData} from './github.js';
+
+let profiles = [];
+
+window.addEventListener('DOMContentLoaded', () => {
+    profiles = loadProfiles();
+    renderProfiles(profiles);
+    updateStats(profiles);
+    loadTheme();
+});
